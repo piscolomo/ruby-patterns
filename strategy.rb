5bb16f82192e44197b4c611d99e2d44187dd332b
Julio Lopez <ljuliom@gmail.com>
2015-03-28 08:54:05 -0500

    strategy pattern

---
+++ strategy.rb
@@ -0,0 +1,48 @@
+class Hero
+  attr_reader :damage, :health, :skills
+  attr_accessor :printer
+
+  def initialize(printer)
+    @damage = 10
+    @health = 5
+    @printer = printer
+    @skills = [:stealth, :driving, :intimidation]
+  end
+
+  def print_stats
+    if block_given?
+      yield(damage, health, skills)
+    else
+      printer.print(damage, health, skills)
+    end
+  end
+end
+
+class BattleStats < HTMLPrinter
+  def print(damage, health, skills)
+    "Damage: #{damage}\nHealth: #{health}"
+  end
+end
+
+class SkillStats < HTMLPrinter
+  def print(damage, health, skills)
+    skills.inject(""){ |result, skill| result + skill.to_s.capitalize + "\n" }
+  end
+end
+
+# Usage
+Hero.new(BattleStats.new)
+Hero.print_stats
+# => Damage: 10
+#    Health: 5
+
+Hero.new(SkillStats.new)
+Hero.print_stats
+# => Stealth
+#    Driving
+#    Intimidation
+
+Hero.new(any_printer)
+Hero.print_stats do |damage, health, skills|
+  "Looks: I'm printing a customize message about my hero with damage #{damage} and number of skills: #{skills.size}"
+end
