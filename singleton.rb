8e8f7aa58618a57778a8715d19aaa35dbe0cc30a
Julio Lopez <ljuliom@gmail.com>
2015-03-28 11:26:50 -0500

    singleton pattern

---
+++ singleton.rb
@@ -0,0 +1,27 @@
+# Define an unique instance of an object
+class HeroFactory
+  @@instance = nil
+
+  def self.instance
+    @@instance = HeroFactory.send(:new) unless @@instance
+    @@instance
+  end
+
+  def create_warrior
+    Warrior.new
+  end
+
+  def create_mage
+    Mage.new
+  end
+
+  private_class_method :new
+end
+
+# Usage
+factory = HeroFactory.instance
+another_factory = HeroFactory.instance
+puts factory == another_factory
+# => true
+HeroFactory.new
+# => Raise Exception
